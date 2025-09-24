b09f5452ced986e477e416bc7ad68ead36f368f2
DreamYa12 <z225687838@gmail.com>
2025-09-24 12:36:53 +0800

    Auto-sync: 2025-09-24 12:36:53

--- src/course/play.rs
+++ src/course/play.rs
@@ -2,6 +2,8 @@ use std::{
     alloc::{Layout, alloc, dealloc, realloc},
     ptr::NonNull,
 };
+
+use rand::seq::index;
 #[derive(Debug)]
 pub struct Vector {
     len: usize,
@@ -28,6 +30,45 @@ impl Vector {
             res.write(val);
         };
         self.len += 1;
+    }
+    ///获取索引处的值
+    pub fn get(&self, index: usize) -> Option<&i32> {
+        if index >= self.len {
+            return None;
+        }
+        let prt = self.data.as_ptr();
+        unsafe {
+            let end = prt.add(index);
+            end.as_ref()
+        }
+    }
+    pub fn set(&mut self, index: usize, val: i32) {
+        if index >= self.len {
+            return;
+        }
+        unsafe {
+            let end = self.data.as_ptr().add(index);
+            end.write(val);
+        }
+    }
+    ///交换索引处的值，当索引不正确的时候直接忽略该该次操作
+    pub fn swap(&mut self, i1: usize, i2: usize) {
+        if i1 >= self.len || i2 >= self.len {
+            return;
+        }
+        let mut _temp = 0;
+        _temp = *self.get(i1).unwrap();
+        let i2_val = self.get(i2).unwrap();
+        self.set(i1, *i2_val);
+        self.set(i2, _temp);
+    }
+    pub fn delete(&mut self, index: usize) -> Option<i32> {
+        //获取当前表的长度，索引是否小于表长度
+        if index >= self.len {
+            return None;
+        }
+
+        todo!()
     }
     pub fn grow(&mut self) {
         let new_cap = self.cap * 2;
@@ -66,6 +107,9 @@ fn test() {
     for i in 0..15 {
         v.push(i);
     }
+    v.swap(0, 1);
     v.print();
+    let val = v.get(1);
+    println!("{}", val.unwrap());
     println!("{:?}", v);
 }
